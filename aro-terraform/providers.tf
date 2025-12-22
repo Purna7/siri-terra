677458ce1248685e8067d8617aabde01444c3f92
Purna7 <purnachandrar@gmail.com>
2025-12-22 21:11:30 +0530

    Update providers.tf

--- aro-terraform/providers.tf
+++ aro-terraform/providers.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   features {}
+client_id       = "0ec0ad90-d8d8-43c1-9a05-d5c2352aae44"
+  client_secret   = "XE8Q~rFpjB0mzfXB-ukhaiUmDTf73FRHL_UVa3I"
+  tenant_id       = "ffdef3a4-6f16-4a1b-8401-39239d1b4f31"
+  subscription_id = "2e89365b-8864-49d5-b5f2-f3bb66426738"
 }
+
