8211571f4fac73858963b69edeb59bb12694f501
Purna7 <purnachandrar@gmail.com>
2025-12-23 00:03:42 +0530

    Update backend.tf

--- aro-terraform/backend.tf
+++ aro-terraform/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<replace-with-tfstate-rg>"
-    storage_account_name = "<replace-with-tfstate-sa>"
-    container_name       = "tfstate"
+    resource_group_name  = "kloudsavvy-commonRG"
+    storage_account_name = "kloudsavvyinfraterraform"
+    container_name       = "terraform-statefile"
     key                  = "aro-terraform.tfstate"
   }
 }
+
