190681f8f894bce23fa4fc7da5ce27f6691b7517
purna7 <purnachandrar@gmail.com>
2025-12-23 18:20:50 +0530

    deleted

--- aro-terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "kloudsavvy-commonRG"
-    storage_account_name = "kloudsavvyinfraterraform"
-    container_name       = "terraform-statefile"
-    key                  = "aro-terraform.tfstate"
-  }
-}
-
