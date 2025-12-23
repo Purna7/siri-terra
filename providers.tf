ad5f2b8dac626ba6e82da0b1de643a58f5ea1e49
purna7 <purnachandrar@gmail.com>
2025-12-23 19:24:58 +0530

    updated

--- providers.tf
+++ providers.tf
@@ -1,7 +1,3 @@
 provider "azurerm" {
   features {}
-  client_id       = var.arm_client_id
-  client_secret   = var.arm_client_secret
-  tenant_id       = var.arm_tenant_id
-  subscription_id = var.arm_subscription_id
 }
